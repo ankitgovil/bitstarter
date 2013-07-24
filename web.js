564dce935e394fed55d43e0bf9b40c30ddca6cb3
root <ankit.18.govil@gmail.com>
2013-07-24 09:33:50 +0000

    changes

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ 
+	var buffer=fs.readFileSync("index.html");
+	
+	response.send(buffer.toString());
+  
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8181;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
